ddd7876d4c2d2e1e4deea68f2bbea5e83c4bc236
c906047 <gospamme@yandex.ru>
2016-11-11 16:51:43 +0300

    Автореферат

--- all.sh
+++ all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+rm -rf *.aux *.bbl *.bcf *.blg *.glg *.glo *.gls *.ilg *.ist *.lof *.log *.lot *.nlo *.nls *.out *.pdf *.xml *.gz *.toc
 /usr/texbin/pdflatex -file-line-error -synctex=1 -shell-escape -enable-write18 -interaction=nonstopmode dissertation.tex
 /usr/texbin/biber dissertation
 /usr/texbin/makeglossaries dissertation.glo
 /usr/texbin/pdflatex -file-line-error -synctex=1 -shell-escape -enable-write18 -interaction=nonstopmode dissertation.tex
 /usr/texbin/makeindex dissertation.nlo -s nomencl.ist -o dissertation.nls
 /usr/texbin/pdflatex -file-line-error -synctex=1 -shell-escape -enable-write18 -interaction=nonstopmode dissertation.tex
+#rm -rf *.aux *.bbl *.bcf *.blg *.glg *.glo *.gls *.ilg *.ist *.lof *.log *.lot *.nlo *.nls *.out  *.xml *.gz *.toc
